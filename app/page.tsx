af9e83b9f403eeb00be9c895737646ed5d128bf1
Morteza Isvand <m.isvand2@gmail.com>
2023-10-17 19:16:35 +0330

    Update fetch api inside of home page.

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ const getPosts = async ({
 }: getSearchParams): Promise<Props[]> => {
   const queryParams = new URLSearchParams(searchParams);
   const res = await fetch(
-    `http://localhost:3000/api/posts${"?" + queryParams.toString()}`,
+    `${process.env.API_URL}/api/posts${"?" + queryParams.toString()}`,
     {
       cache: "no-store",
     }
